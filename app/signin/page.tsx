758b8ccbe63ac9a6129668c1a56150a7373c5673
nagarajsita <nagarajsita@gmail.com>
2025-04-09 21:13:25 +0530

    callback url fixed

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -6,7 +6,7 @@ export default function SignIn() {
     <div className="flex justify-center items-center h-screen bg-grid-gray-100">
       <form action={async ()=>{
         "use server"
-        await signIn("github", { callbackurl: "/dashboard" });
+        await signIn("github",{ callbackUrl: "/dashboard" });
       }} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-100">
         <h2 className="text-3xl mb-10 text-center">Choose Method to Login/Signup</h2>
         <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
